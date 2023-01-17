0e2a6607d76635bc26bc7a517eccc3cb72f755de
kimhojeon <hojeon0629@gmail.com>
2023-01-17 17:11:25 +0900

    개인프로젝트1 스플래시, 로그인화면

---
+++ app/src/main/java/com/example/ex0113/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.ex0113
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        var handler = Handler()
+        handler.postDelayed({
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 3000)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+    }
