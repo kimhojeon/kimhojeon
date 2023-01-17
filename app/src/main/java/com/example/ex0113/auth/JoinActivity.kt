e3e07a9ba4b29f860672c88f14ddf914554b85a0
kimhojeon <hojeon0629@gmail.com>
2023-01-18 00:26:52 +0900

    개인프로젝트1 스플래시, 로그인화면  순서 전환, 파이어베이스 전

---
+++ app/src/main/java/com/example/ex0113/auth/JoinActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ex0113.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ex0113.R
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
